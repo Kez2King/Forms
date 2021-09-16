46be25a08b63f1bf627d4481428af10dabf0425e
Mar'Kez <MarkezBJC@gmail.com>
2021-09-16 15:39:43 -0400

    Javascript Intro

---
+++ Javascript/apps.js
@@ -0,0 +1,19 @@
+// Single Line Comment...
+/**
+ * Multi-Line Comment ..
+ */
+console.log("External JS file")
+console.log(typeof 12345)
+
+console.log(typeof 123.45)
+
+console.log(typeof true)
+
+console.log(typeof new Date())
+
+console.log(typeof null)
+
+ function printName(){
+    console.log("King")
+ }
+printName()
